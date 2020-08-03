72079d8feb1e9f0198ca1bc3709a0297a24bb394
John McNay <john.w.mcnay@gmail.com>
2020-08-03 08:11:44 -0500

    started to add keyboard inputs

--- js/calculator.js
+++ js/calculator.js
@@ -7,10 +7,43 @@
 //     });
 // });
 
-var screenText = '0.';
+// var screenText = '0';
 var freshScreen = true;
+const zeroKey = 48, nineKey = 57;
+const plusKey = 187, minusKey = 189, divideKey = 191; timesKey = 88;
+const operatorKeys = [187, 189, 191, 88];
 
-function enterNumber(num) {
+const decimalPressed = key => {
+    return (key === 190);
+};
+
+//returns whether or not the key code is for the + - x / operators
+const isAnOperator = key => {
+    return (operatorKeys.indexOf(key) !== -1);
+};
+
+//returns true or false if the argument is a keyCode between 0 and 9
+const pressedANumber = key => {
+    return (key >= zeroKey && key <= nineKey);
+};
+
+//keydown event to handle keyboard inputs as well
+$(document).keydown(function(keyPressed) {
+    const key = keyPressed.keyCode;
+
+    console.log(key);
+
+    if (pressedANumber(key)) {
+        handleNumber(key - zeroKey);
+    } else if (isAnOperator(key)) {
+        handleOperator(key);
+    } else if (decimalPressed(key)) {
+        handleDecimal()
+    }
+});
+
+//updates the screen based on which number is pressed
+function handleNumber(num) {
 
     //var test = 'the test worked';
     //receives input based on button clicked
@@ -24,8 +57,30 @@ function enterNumber(num) {
         if (freshScreen) {
             currentText = ''
             freshScreen = false;
+        } else {
+            if (num === 0) {
+                console.log("zero pressed");
+                //handleZero()
+            } else {
+                $("#screen").text(currentText + num);
+            }
         }
-        
-        $("#screen").text(currentText + num);
+
+
     });
 }
+
+//clears the screen wiping out any progress
+const clearScreen = () => {
+    freshScreen = true;
+    $("#screen").text('0')
+};
+
+//+ - / x is passed in and handled to update the screen and calculations
+const handleOperator = op => {
+    console.log("handleOperator ran");
+};
+
+const handleDecimal = () => {
+    console.log("decimal pressed");
+};
