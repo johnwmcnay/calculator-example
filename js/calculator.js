8f389b537cae8949c2fc48d704d6ffe40bd6abec
John McNay <john.w.mcnay@gmail.com>
2020-08-02 20:01:21 -0500

    added jQuery, tested onclick function

--- js/calculator.js
+++ js/calculator.js
@@ -1,2 +1,24 @@
 // TODO add function to update the field and perform calculations
 
+// jQuery Example
+// $(function(){
+//     $("#screen").click(function(){
+//         $(this).hide();
+//     });
+// });
+
+var screenText = '0.';
+
+function enterNumber(num) {
+
+    var test = 'the test worked';
+    //receives input based on button clicked
+    // either:
+    //    -a numeral is pressed updating the number
+    //    -a calculation is pressed, arithmetic is performed, update the number
+    //    -clear is pressed and makes the number 0
+
+    $(function(){
+        $("#screen").text(test);
+    });
+}
