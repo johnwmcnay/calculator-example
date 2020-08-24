c4a4346b5e74581e8784f2b8982f0c4877a5572b
John McNay <john.w.mcnay@gmail.com>
2020-08-23 19:31:34 -0500

    starting to work on a calculator layout creator

--- js/calculator-class.js
+++ js/calculator-class.js
@@ -4,7 +4,8 @@
 //const zeroKey = 48, nineKey = 57;
 //const plusKey = 187, minusKey = 189, divideKey = 191; timesKey = 88;
 
-//calculator has a screen, keypad, log
+//TODO: build calculator from a layout
+// calculator has a screen, keypad, log
 class calculator {
     constructor() {
         this.createCalculator();
@@ -19,12 +20,16 @@ class calculator {
         this.log = new calculatorLog();
         let element = document.createElement("div");
         element.id = "calculator";
-        this.HTML = element;
+        //add screen
+        //add keypad
+        //add log
+        //this.HTML = all of that;
     }
 
 }
 
 //has inputField
+//TODO: adjusts font size based how much data is in the field, using a monospace font
 class calculatorScreen {
     constructor() {
         this.freshScreen = true;
@@ -37,11 +42,21 @@ class calculatorScreen {
 //     }
 // }
 
-//has key objects: numbers, operators
-//has a layout
+//TODO: build from a layout
+// has key objects: numbers, operators
 class calculatorKeypad {
     constructor() {
+        this.createKeypad();
+    }
 
+    //layout example
+    //789CC
+    //456+-
+    //123x/
+    //00.==
+
+    createKeypad() {
+        //create all the keys and place organize them accordingly to the layout
     }
 
     createKey(key) {
@@ -80,3 +95,46 @@ class calculatorLog {
 
     }
 }
+
+//TODO: solve how to create the keypad from a keymap layout
+//keymap layout
+let keymap = [
+    "789CC",
+    "456+-",
+    "123x/",
+    "00.=="
+    ]
+
+
+
+let keypad = document.createElement("div");
+keypad.classList.add("keypad");
+
+//TODO: get pencil and paper to figure out algorithm;
+// left to right, find all columns greater than one row tall;
+// design a creation tool instead??? keymap has a hard algorithm and is less intuitive to use
+// for (let rowIndex in keymap) {
+//     let row = document.createElement("div")
+//     row.classList.add("row");
+//
+//     for (let columnIndex in keymap[rowIndex]) {
+//
+//     }
+// }
+
+
+//TODO: work on creating an editor to design a calculator
+let createField = document.createElement("div");
+createField.style.display = "flex";
+createField.style.flexDirection = "column";
+
+for (let row = 1; row <= 8; row++) {
+    let element = document.createElement("div");
+
+    for (let col = 1; col <= 16; col++) {
+        let btn = document.createElement("button");
+        element.appendChild(btn);
+    }
+    createField.appendChild(element);
+}
+// document.body.appendChild(createField);
